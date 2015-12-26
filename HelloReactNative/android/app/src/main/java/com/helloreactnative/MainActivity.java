9bc8c8b631279c088c7552d10801b0cfd3a4b401
xubinggui <xubinggui@zepplabs.com>
2015-12-26 17:12:32 +0800

    run UIApplicationExplore

--- HelloReactNative/android/app/src/main/java/com/helloreactnative/MainActivity.java
+++ HelloReactNative/android/app/src/main/java/com/helloreactnative/MainActivity.java
@@ -3,13 +3,11 @@ package com.helloreactnative;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-
 import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
@@ -72,7 +70,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         super.onResume();
 
         if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
+            mReactInstanceManager.onResume(this, this);
         }
     }
 }
