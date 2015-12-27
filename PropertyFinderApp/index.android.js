38d52a6cbdf195a841d48e50babcd14d2f4f2a38
xubinggui <xubinggui@zepplabs.com>
2015-12-27 18:39:49 +0800

    update PropertyFinderApp

--- PropertyFinderApp/index.android.js
+++ PropertyFinderApp/index.android.js
@@ -5,49 +5,29 @@
 'use strict';
 
 var React = require('react-native');
+var SearchPage = require('./searchPage');
+
 var {
   AppRegistry,
-  StyleSheet,
+  BackAndroid,
   Text,
   View,
+  Navigator,
+  StyleSheet,
+  ToolbarAndroid,
+  ToastAndroid,
 } = React;
 
+var styles = React.StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
 
 var PropertyFinderApp = React.createClass({
   render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return <SearchPage />
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('PropertyFinderApp', () => PropertyFinderApp);
+React.AppRegistry.registerComponent('PropertyFinderApp', function() { return PropertyFinderApp });
